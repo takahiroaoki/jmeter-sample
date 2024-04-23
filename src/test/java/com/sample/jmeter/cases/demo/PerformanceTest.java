e6fcbbc1befc2114a0c34aafadc1c6a3fcc9e521
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2024-04-24 00:05:41 +0900

    refactor

--- src/test/java/com/sample/jmeter/cases/demo/PerformanceTest.java
+++ src/test/java/com/sample/jmeter/cases/demo/PerformanceTest.java
@@ -8,47 +8,19 @@ import java.time.Duration;
 import org.junit.jupiter.api.Test;
 
 import com.sample.jmeter.constants.PathConst;
-import com.sample.jmeter.constants.UrlConst;
+import com.sample.jmeter.thread_groups.Demo;
 
 public class PerformanceTest {
-    /* INPUT */
-    private static final String PROTOCOL_DATA_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/data/protocol.txt";
-    private static final String DOMAIN_DATA_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/data/domain.txt";
-    private static final String PORT_DATA_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/data/port.txt";
-    private static final String API_KEY_DATA_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/data/api_key.txt";
-    private static final String PARAMS_DATA_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/data/params.csv";
-
     /* OUTPUT */
     private static final String REPORT_OUTPUT_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/report.csv";
 
     @Test
     void test() throws IOException {
-        testPlan(
-                vars()
-                        .set("protocol", String.format(PathConst.DATA_PATH_FORMATTER,
-                                PROTOCOL_DATA_PATH))
-                        .set("domain", String.format(PathConst.DATA_PATH_FORMATTER,
-                                DOMAIN_DATA_PATH))
-                        .set("port", String.format(PathConst.DATA_PATH_FORMATTER,
-                                PORT_DATA_PATH))
-                        .set("api-key", String.format(PathConst.DATA_PATH_FORMATTER,
-                                API_KEY_DATA_PATH)),
-                csvDataSet(PARAMS_DATA_PATH))
+        testPlan()
                 .tearDownOnlyAfterMainThreadsDone()
                 .children(
                         httpCache().disable(),
-                        threadGroup("ThreadGroup0")
-                                .rampToAndHold(5, Duration.ofSeconds(5),
-                                        Duration.ofSeconds(10))
-                                .children(
-                                        throughputTimer(60.0),
-                                        httpSampler("${protocol}://${domain}:${port}"
-                                                + UrlConst.DEMO_ENDPOINT)
-                                                .header("api-key",
-                                                        "${api-key}")
-                                                .param("paramA", "${PARAM_A}")
-                                                .param("paramB", "${PARAM_B}"),
-                                        resultsTreeVisualizer()),
+                        Demo.getThreadGroup("demo", 2, Duration.ofSeconds(2L), Duration.ofSeconds(20L)),
                         jtlWriter("", REPORT_OUTPUT_PATH))
                 .saveAsJmx(PathConst.JMX_OUTPUT_CONTEXT + "demo/case.jmx");
     }
