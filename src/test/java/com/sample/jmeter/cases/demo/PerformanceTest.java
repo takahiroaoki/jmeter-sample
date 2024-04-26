f696a422107f813ef1ab74c1bde025b8a569cae6
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2024-04-26 20:01:25 +0900

    formatting

--- src/test/java/com/sample/jmeter/cases/demo/PerformanceTest.java
+++ src/test/java/com/sample/jmeter/cases/demo/PerformanceTest.java
@@ -13,15 +13,15 @@ import com.sample.jmeter.thread_groups.Demo;
 public class PerformanceTest {
     /* OUTPUT */
     private static final String REPORT_OUTPUT_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/report.csv";
-
+    
     @Test
     void test() throws IOException {
         testPlan()
-                .tearDownOnlyAfterMainThreadsDone()
-                .children(
-                        httpCache().disable(),
-                        Demo.getThreadGroup("demo", 2, Duration.ofSeconds(2L), Duration.ofSeconds(20L)),
-                        jtlWriter("", REPORT_OUTPUT_PATH))
-                .saveAsJmx(PathConst.JMX_OUTPUT_CONTEXT + "demo/case.jmx");
+            .tearDownOnlyAfterMainThreadsDone()
+            .children(
+                httpCache().disable(),
+                Demo.getThreadGroup("demo", 2, Duration.ofSeconds(2L), Duration.ofSeconds(20L)),
+                jtlWriter("", REPORT_OUTPUT_PATH))
+            .saveAsJmx(PathConst.JMX_OUTPUT_CONTEXT + "demo/case.jmx");
     }
 }
--- src/test/java/com/sample/jmeter/thread_groups/Demo.java
+++ src/test/java/com/sample/jmeter/thread_groups/Demo.java
@@ -16,25 +16,25 @@ public class Demo {
     private static final String PORT_DATA_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/data/port.txt";
     private static final String API_KEY_DATA_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/data/api_key.txt";
     private static final String PARAMS_DATA_PATH = PathConst.JMX_OUTPUT_CONTEXT + "demo/data/params.csv";
-
+    
     public static DslDefaultThreadGroup getThreadGroup(
-            String threadGroupName,
-            int threadsPerGroup,
-            Duration rampUpDuration,
-            Duration holdDuration) {
+        String threadGroupName,
+        int threadsPerGroup,
+        Duration rampUpDuration,
+        Duration holdDuration) {
         return threadGroup(threadGroupName).rampToAndHold(threadsPerGroup, rampUpDuration, holdDuration)
-                .children(
-                        vars()
-                                .set("protocol", String.format(PathConst.DATA_PATH_FORMATTER, PROTOCOL_DATA_PATH))
-                                .set("domain", String.format(PathConst.DATA_PATH_FORMATTER, DOMAIN_DATA_PATH))
-                                .set("port", String.format(PathConst.DATA_PATH_FORMATTER, PORT_DATA_PATH))
-                                .set("api-key", String.format(PathConst.DATA_PATH_FORMATTER, API_KEY_DATA_PATH)),
-                        csvDataSet(PARAMS_DATA_PATH),
-                        throughputTimer(60.0),
-                        httpSampler("${protocol}://${domain}:${port}" + UrlConst.DEMO_ENDPOINT)
-                                .header("api-key", "${api-key}")
-                                .param("paramA", "${PARAM_A}")
-                                .param("paramB", "${PARAM_B}"),
-                        resultsTreeVisualizer());
+            .children(
+                vars()
+                    .set("protocol", String.format(PathConst.DATA_PATH_FORMATTER, PROTOCOL_DATA_PATH))
+                    .set("domain", String.format(PathConst.DATA_PATH_FORMATTER, DOMAIN_DATA_PATH))
+                    .set("port", String.format(PathConst.DATA_PATH_FORMATTER, PORT_DATA_PATH))
+                    .set("api-key", String.format(PathConst.DATA_PATH_FORMATTER, API_KEY_DATA_PATH)),
+                csvDataSet(PARAMS_DATA_PATH),
+                throughputTimer(60.0),
+                httpSampler("${protocol}://${domain}:${port}" + UrlConst.DEMO_ENDPOINT)
+                    .header("api-key", "${api-key}")
+                    .param("paramA", "${PARAM_A}")
+                    .param("paramB", "${PARAM_B}"),
+                resultsTreeVisualizer());
     }
 }
